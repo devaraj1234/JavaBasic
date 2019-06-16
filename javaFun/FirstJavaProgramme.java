efd4a42236f57e3ac9409dd9f4752d5de873a1e5
git1 <dev.acharya286@gmail.com>
2019-06-16 14:03:41 -0500

    initial commit for first java programme

---
+++ javaFun/FirstJavaProgramme.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgramme{
+	public static void main(String[] args){
+		System.out.println("My name is Dev Acharya");
+		System.out.println("I am -- years old.");
+		System.out.println("My hometown is Irving, Texas.");
+	}
+}
+
